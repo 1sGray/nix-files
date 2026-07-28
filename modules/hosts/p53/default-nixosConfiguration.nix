40ebaa3acd654b00e180c31fe141d4ebc9f8d276
1sGray <1s_gray@proton.me>
2026-07-27 23:45:30 -0500

    added brave

--- modules/hosts/p53/default-nixosConfiguration.nix
+++ modules/hosts/p53/default-nixosConfiguration.nix
@@ -66,6 +66,7 @@
 		  neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
 		  wget
 		  git
+		  brave
 		];
 		
 		nix.settings.experimental-features = [ "nix-command" "flakes" ];
