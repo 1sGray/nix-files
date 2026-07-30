6c209a1a6c43f3e9517dc42135f33a82efffdc20
1sGray <1s_gray@proton.me>
2026-07-30 17:15:22 -0500

    bruh

--- modules/features/zellij/zellij.nix
+++ modules/features/zellij/zellij.nix
@@ -1,28 +1,30 @@
-{ self, inputs, ... }: {
-	imports = [ inputs.wrapper-modules.flakeModules.wrappers ];
+{ self, inputs, lib, wlib, ... }: {
+	# imports = [ inputs.wrapper-modules.flakeModules.wrappers ];
 
-	flake.nixosModules.zellij = { pkgs, lib, self', ... }: {
-		programs.zellij = {
-			enable = true;
-			package = self'.packages.myZellij;
-		};
-	};
+	# flake.nixosModules.zellij = { pkgs, lib, self', ... }: {
+	# 	programs.zellij = {
+	# 		enable = true;
+	# 		package = self'.packages.myZellij;
+	# 	};
+	# };
 	
-	perSystem = { pkgs, lib, wlib, config, inputs', ... }: {
+	perSystem = { pkgs, lib, wlib, config, inputs', self', ... }: {
 
-		packages.myZellij = inputs'.wrapper-modules.wrappers.zellij.wrap {
+		packages.myZellij = inputs'.wrapper-modules.lib.wrapPackages ({ config, lib, pkgs, ... }: {
 			inherit pkgs;
 
-			imports = [ wlib.modules.default ];
+			# imports = [ wlib.modules.default ];
+
+
 
 			options.settings.config_directory = lib.mkOption {
 				type = lib.types.path;
 				default = ./configs/.config/zellij;
 				description = "Directory containing config.kdl and layouts/ for zellij.";
 			};
-
-			config.package = lib.mkDefault pkgs.zellij;
+			package = inputs'.zellij.packages.default;
+			# config.package = lib.mkDefault pkgs.zellij;
 			config.envDefault.ZELLIJ_CONFIG_DIR = "${config.settings.config_directory}";
-		};
+		});
 	};
 }
