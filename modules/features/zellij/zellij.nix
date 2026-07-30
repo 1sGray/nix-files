266679c1b319cf568e52601c1f7962b0e001390b
1sGray <1s_gray@proton.me>
2026-07-30 17:40:24 -0500

    brother.

--- modules/features/zellij/zellij.nix
+++ modules/features/zellij/zellij.nix
@@ -10,21 +10,31 @@
 	
 	perSystem = { pkgs, lib, wlib, config, inputs', self', ... }: {
 
-		packages.myZellij = inputs'.wrapper-modules.lib.wrapPackages ({ config, lib, pkgs, ... }: {
-			inherit pkgs;
+		# packages.myZellij = inputs'.wrapper-modules.lib.wrapPackages ({ config, lib, pkgs, ... }: {
+		# 	inherit pkgs;
+		#
+		# 	# imports = [ wlib.modules.default ];
+		#
+		#
+		#
+		# 	options.settings.config_directory = lib.mkOption {
+		# 		type = lib.types.path;
+		# 		default = ./configs/.config/zellij;
+		# 		description = "Directory containing config.kdl and layouts/ for zellij.";
+		# 	};
+		# 	package = inputs'.zellij.packages.default;
+		# 	# config.package = lib.mkDefault pkgs.zellij;
+		# 	config.envDefault.ZELLIJ_CONFIG_DIR = "${config.settings.config_directory}";
+		# });
 
-			# imports = [ wlib.modules.default ];
-
-
-
-			options.settings.config_directory = lib.mkOption {
-				type = lib.types.path;
-				default = ./configs/.config/zellij;
-				description = "Directory containing config.kdl and layouts/ for zellij.";
-			};
-			package = inputs'.zellij.packages.default;
-			# config.package = lib.mkDefault pkgs.zellij;
-			config.envDefault.ZELLIJ_CONFIG_DIR = "${config.settings.config_directory}";
-		});
+		packages.myZellij = pkgs.symlinkJoin {
+			name = "zellij";
+			paths = [ pkgs.zellij ];
+			nativeBuildInputs = [ pkgs.makeWrapper ];
+			postBuild = ''
+				wrapProgram $out/bin/zellij \
+					--set-default ZELLIJ_COFIG_DIR ${./configs/.config/zellij}
+			'';
+		};
 	};
 }
