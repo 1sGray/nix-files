e5d71b36fb594f0322c33270fc3a9b4341268b7b
1sGray <1s_gray@proton.me>
2026-08-03 06:43:32 -0500

    starship installed, configuation needed

--- modules/features/starship-prompt/starship.nix
+++ modules/features/starship-prompt/starship.nix
@@ -1,13 +1,13 @@
 { self, inputs, ... }: {
 	
-	flake.nixModules.starship = { pkgs, self, ... }: let
+	flake.nixosModules.starship = { pkgs, self, ... }: let
 		currentSystem = pkgs.stdenv.hostPlatform.system;
 	in{
-		envirnoment.systemPackages = [ self.packages.${currentSystem}.myStarship ];
+		environment.systemPackages = [ self.packages.${currentSystem}.myStarship ];
 	};
 	
 	perSystem = { pkgs, lib, self, ... }: {
-		packages.myStarshipPrompt = inputs.wrapper-modules.wrappers.starship.wrap {
+		packages.myStarship = inputs.wrapper-modules.wrappers.starship.wrap {
 			inherit pkgs;
 			env.STARSHIP_CONFIG = lib.mkForce (toString ./configs/.config/starship.toml);
 		};
