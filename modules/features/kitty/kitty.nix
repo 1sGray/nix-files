3f1386d5f88b067f477befa91efda5e94a9a23f3
1sGray <1s_gray@proton.me>
2026-08-04 12:17:53 -0500

    configuring niri, setting up kitty

---
+++ modules/features/kitty/kitty.nix
@@ -0,0 +1,11 @@
+{ self, inputs, ... }: {
+	
+	# flake.nixModules.template = { pkgs, lib, ... }: {};
+
+	perSystem = { pkg , lib, ... }: {
+		packages.myKitty = inputs.wrapper-modules.wrappers.kitty.wrap {
+			inherit pkgs;
+			addFlag = [ "--config" ./configs/.config/kitty/kitty.conf ];
+		};
+	};
+}
