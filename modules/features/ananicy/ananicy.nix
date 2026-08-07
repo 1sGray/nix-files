cacb3a6cbce5df986453e36d12c27230bb18f895
1sGray <1s_gray@proton.me>
2026-08-07 17:00:32 -0500

    Create ananicy.nix

---
+++ modules/features/ananicy/ananicy.nix
@@ -0,0 +1,8 @@
+
+{
+    services.ananicy = {
+        enable = true;
+        package = pkgs.ananicy-cpp; # ananicy is originally a python program, the c++ rewrite uses less resources
+        rulesProvider = pkgs.ananicy-rules-cachyos;
+    };
+}
