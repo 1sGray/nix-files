48d0c82b5b265ea4ade24d61f4a2bf0262ba92bf
1sGray <1s_gray@proton.me>
2026-08-07 17:31:22 -0500

    added niri fork repo

--- flake.nix
+++ flake.nix
@@ -4,14 +4,19 @@
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
+    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
+
+    niri-urayde = { # Niri fork the supports screen tearing
+        url = "github:urayde/niri";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     minegrub-world-sel-theme.url = "github:Lxtharia/minegrub-world-sel-theme";
     noctalia = {
     	url = "github:noctalia-dev/noctalia";
     };
 
-    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
   };
 
   # import modules/ automatically
