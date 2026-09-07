d3ec4d699cff7da1ce5e7a40580164950c1fb52f
gray <1s_gray@proton.me>
2026-09-07 12:02:58 -0500

    yazi plugins annoy me

--- modules/features/yazi/yazi.nix
+++ modules/features/yazi/yazi.nix
@@ -1,38 +1,21 @@
 # modules/features/yazi/yazi.nix
 { self, inputs, ... }: {
 
-	flake.nixosModules.yazi = { pkgs, lib, username, ... }: let
+    flake.nixosModules.yazi = { pkgs, lib, username, ... }:{
 
-		configDir = ./configs/.config/yazi;
-		homeDir = "/home/${username}";
-		yaziConfigDir = "${homeDir}/.config/yazi";
+        programs.yazi = {
 
-		# nixpkgs-packaged plugins to symlink in. Add/remove entries here.
-		plugins = with pkgs.yaziPlugins; {
-			full-border = full-border;
-			# smart-enter = smart-enter;
-			# git = git;
-		};
+            enable = true;
 
-		pluginLinkRules = lib.mapAttrsToList (name: drv:
-			"L+ ${yaziConfigDir}/plugins/${name}.yazi - ${username} users - ${drv}"
-		) plugins;
+            plugins = {
+                # ".yazi" = pkgs.yaziPlugins.
 
-	in {
-		environment.systemPackages = [ pkgs.yazi ];
+                full-border = pkgs.yaziPlugins.full-border;
+                mount =       pkgs.yaziPlugins.mount;
 
-		systemd.tmpfiles.rules = [
-			"d ${yaziConfigDir} 0755 ${username} users -"
-			"d ${yaziConfigDir}/plugins 0755 ${username} users -"
+            };
+        };
 
-			# Static, Nix-managed files
-			"L+ ${yaziConfigDir}/yazi.toml - ${username} users - ${configDir}/yazi.toml"
-			"L+ ${yaziConfigDir}/keymap.toml - ${username} users - ${configDir}/keymap.toml"
-			"L+ ${yaziConfigDir}/init.lua - ${username} users - ${configDir}/init.lua"
-
-			# Deliberately NOT managing theme.toml here — noctalia/matugen
-			# owns that file directly and writes it live.
-		] ++ pluginLinkRules;
-	};
+    };
 
 }
