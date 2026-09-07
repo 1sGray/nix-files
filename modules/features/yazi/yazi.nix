4665d4eecb613abb93b4bedf66b005ba35698434
gray <1s_gray@proton.me>
2026-09-07 12:40:56 -0500

    kinda fixed yazi?

--- modules/features/yazi/yazi.nix
+++ modules/features/yazi/yazi.nix
@@ -14,6 +14,18 @@
                 mount =       pkgs.yaziPlugins.mount;
 
             };
+
+
+            # Import configs directly from your local files
+            settings = {
+                yazi = lib.importTOML ./configs/.config/yazi/yazi.toml;
+                keymap = lib.importTOML   ./configs/.config/yazi/keymap.toml;
+                # theme = lib.importTOML    ./configs/.config/yazi/theme.toml;
+            };
+
+            # If you need to initialize a Lua script
+            # initLua = builtins.readFile ./configs/.config/yazi/init.lua;
+
         };
 
     };
